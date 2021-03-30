5e3d45049a83574b1d0d2b0f8c64809c498c456c
Tirth Raj Kumar <stirthraj.12345@gmail.com>
2021-03-30 21:43:32 +0530

    google search

---
+++ src/components/Search.js
@@ -0,0 +1,44 @@
+import search from "../img/search.svg";
+import mic from "../img/googlemic_clr_24px.svg";
+import logo from "../img/google_logo.svg";
+import { useEffect, useState } from "react";
+import SearchData from "./SearchData";
+import '../css/Search.css';
+
+function Search() {
+  const [query, setQuery] = useState("");
+  const onKeyUpHandle = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      setQuery(e.target.value);
+    }
+  };
+  useEffect(() => {
+    document.title = `Result of ${query}`;
+  }, [query]);
+
+  if (query === "") {
+    return (
+      <>
+        <div className="Search">
+          <div>
+            <img className="Search-logo" src={logo} alt="logo" />
+          </div>
+          <div className="Search-bar">
+            <img className="Search-bar-prev" src={search} alt="icon" />
+            <input
+              className="Search-input"
+              defaultValue={query}
+              onKeyUp={onKeyUpHandle}
+              placeholder="Search Google or type a URL"
+            />
+            <img className="Search-bar-next" src={mic} alt="mic-icon" />
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return <SearchData query={query} />;
+  }
+}
+export default Search;
---
+++ src/components/SearchData.js
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from 'react';
+import '../css/SearchData.css';
+import search from "../img/search.svg";
+import mic from "../img/googlemic_clr_24px.svg";
+import logo from "../img/google_logo.svg";
+
+function SearchData(props) {
+  const [query,setQuery]=useState(props.query);
+  const [result,setResult]=useState([]);  
+
+  const qq=useRef();
+  const onKeyUpHandle = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      setQuery(e.target.value);
+    }
+  };
+  const API_KEY = 'AIzaSyDrLZZoy4wQ9 - t0dJvWAfMxWlhfvdeXdak';
+  const CONTEXT_KEY = 'b285d02aa413dc4f4';
+
+  useEffect(() => {
+    document.title = `Result of ${query}`;
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${query}`)
+    .then(res=>res.json())
+    .then(res=>{
+      setResult(res.items);
+    })
+  }, [query]);
+
+  return (
+    <>
+      <div className="Sticky">
+        <div>
+          <img className="Sticky-logo" src={logo} alt="logo" />
+        </div>
+        <div className="Sticky-bar">
+          <input
+            ref={qq}
+            className="Sticky-input"
+            size="50"
+            defaultValue={query}
+            onKeyUp={onKeyUpHandle}
+            placeholder="Search Google or type a URL"
+          />
+          <button
+            className="Sticky-btn"
+            onClick={() => setQuery(qq.current.value)}
+          >
+            <img className="Sticky-bar-search" src={search} alt="icon" />
+          </button>
+          <img className="Sticky-bar-mic" src={mic} alt="mic-icon" />
+        </div>
+        <div className="Sign-up">
+          <button className="Sign-btn">Sign in</button>
+        </div>
+      </div>
+      <div className="container">
+        {result.map((res) => (
+          <div key={res.id} className="card">
+            <div style={{ margin: "10px" }}>
+              <div className="card-link">{res.formattedUrl}</div>
+              <a href={res.formattedUrl}>
+                <h3
+                  className="card-title"
+                  dangerouslySetInnerHTML={{ __html: res.htmlTitle }}
+                />
+              </a>
+              <div>{res.snippet}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="footer">
+        <h4>KloudOne &copy; @2021-2022</h4>
+      </div>
+    </>
+  );
+}
+export default SearchData;
